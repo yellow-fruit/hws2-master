2ba1930124860a5b031633c8959b6d379be98ff1
Nikolai_Golikov <golikov4nikolai@gmail.com>
2023-07-12 17:17:09 +0300

    hw3 fixed not error

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -51,7 +51,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
         setName(e.currentTarget.value) // need to fix
 
-        error && setError('')
+        /*error && setError('')*/
     }
     const addUser = () => {
         pureAddUser(name, setError, setName, addUserCallback)
