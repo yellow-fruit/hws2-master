2f1541358135049fead3d144e64a4199d3cc29f4
Nikolai_Golikov <golikov4nikolai@gmail.com>
2023-07-13 11:52:47 +0300

    hw3 fixed s.errorInput4

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -26,7 +26,7 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = s.errorInput
+    const inputClass = error
         ? s.errorInput
         : s.input
    // need to fix with (?:)
