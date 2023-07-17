565b4cede72f46ff9684edeab5fa9994059a8a1f
Nikolai_Golikov <golikov4nikolai@gmail.com>
2023-07-17 16:41:51 +0300

    hw5.1

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Routes, Route, Navigate, NavLink} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -14,6 +14,7 @@ export const PATH = {
 function Pages() {
     return (
         <div>
+
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
                 {/*роутинг будут писать студенты*/}
